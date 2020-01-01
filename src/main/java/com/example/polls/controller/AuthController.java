0de0f3eee5a20f14658f599a5cc26b0740087e4d
karthikreddy521 <CHINTU@KarthikeddysMBP.fios-router.home>
2020-01-01 18:41:41 -0500

    added yaml file for mysql connection fix on aws

--- src/main/java/com/example/polls/controller/AuthController.java
+++ src/main/java/com/example/polls/controller/AuthController.java
@@ -19,10 +19,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -65,6 +62,12 @@ public class AuthController {
 
         String jwt = tokenProvider.generateToken(authentication);
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    @GetMapping("/test")
+    public String myTest()
+    {
+        return "working !!";
     }
 
     @PostMapping("/signup")
